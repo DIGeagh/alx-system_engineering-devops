d30a47b0afe94f25b01d37c26e6b3747b70505e3
DIGeagh <darlingtongavi@gmail.com>
2023-08-01 14:34:00 -0700

    Regular expression

--- 0x06-regular_expressions/5-beginning_and_end.rb
+++ 0x06-regular_expressions/5-beginning_and_end.rb
@@ -1,4 +1,19 @@
-#!/usr/bin/env bash
-# Kills the 4-to_infinity_and_beyond processi.
+#!/usr/bin/env ruby
 
-kill "$(pgrep -f 4-to_infinity_and_beyond)"
+# Check if the script is called with an argument
+if ARGV.empty?
+  puts "Usage: #{$0} <string>"
+  exit 1
+end
+
+# Get the first argument passed to the script
+input_string = ARGV[0]
+
+# Define the regular expression to match the pattern
+regex = /^h.n$/
+
+# Use the `match` method of the regular expression to find the match in the input string
+match_result = input_string.match(regex)
+
+# Output the matched string (or an empty string if no match is found)
+puts match_result ? match_result[0] : ""

ae31e1935629bec29928077296569d92414c23e9
DIGeagh <darlingtongavi@gmail.com>
2023-08-04 14:28:42 -0700

    0x06-regular_expressions

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,19 +1,2 @@
-#!/bin/bash
-
-# Function to extract information from log entries
-extract_info() {
-    # Regular expression to match the required fields (Oniguruma ERE)
-    local regex='\[from:(.*?)\] \[to:(.*?)\] \[flags:(.*?)\]'
-    if [[ $1 =~ $regex ]]; then
-        echo "${BASH_REMATCH[1]},${BASH_REMATCH[2]},${BASH_REMATCH[3]}"
-    fi
-}
-
-# Check if the script was given the correct number of arguments
-if [[ $# -ne 1 ]]; then
-    echo "Usage: $0 '<log entry>'"
-    exit 1
-fi
-
-# Call the function with the provided log entry
-extract_info "$1"`:wq
+#!/usr/bin/env ruby
+puts ARGV[0].scan(/(?<=from:|to:|flags:).+?(?=\])/).join(',')

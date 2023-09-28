f03a3cf9ed5549e86af7004ccf2a1b3f984f7da0
DIGeagh <darlingtongavi@gmail.com>
2023-09-27 19:01:49 -0700

    configuration_management

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,6 @@
-# install puppet-lint -v 2.1.0
-
-exec { 'puppet-lint':
-  command => '/usr/bin/apt-get -y install puppet-lint -v 2.1.0',
+# Puppet manifest to install Flask version 2.1.0 using pip3
+exec { 'install_flask':
+  command => '/usr/bin/pip3 install Flask==2.1.0',
+  path    => ['/usr/bin'],
+  creates => '/usr/local/bin/flask', # Adjust the path if needed
 }

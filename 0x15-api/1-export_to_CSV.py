562f30eac1428e723f742b8fb9bb354e4559e555
DIGeagh <darlingtongavi@gmail.com>
2023-10-23 21:00:04 -0700

    API

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,47 +1,33 @@
 #!/usr/bin/python3
 """
 Using https://jsonplaceholder.typicode.com
-returns info about employee TODO progress and exports to CSV
+gathers data from API and exports it to CSV file
 Implemented using recursion
 """
-import csv
+import re
 import requests
 import sys
 
-def export_to_csv(employee_id, user_name, todos_res):
-    filename = f"{employee_id}.csv"
 
-    with open(filename, mode='w', newline='') as csv_file:
-        csv_writer = csv.writer(csv_file, quoting=csv.QUOTE_MINIMAL)
+API = "https://jsonplaceholder.typicode.com"
+"""REST API url"""
 
-        # Write the CSV header
-        csv_writer.writerow(["USER_ID", "USERNAME", "TASK_COMPLETED_STATUS", "TASK_TITLE"])
 
-        for todo in todos_res:
-            csv_writer.writerow([str(employee_id), user_name, str(todo['completed']), todo['title']])
-
-if __name__ == '__main':
+if __name__ == '__main__':
     if len(sys.argv) > 1:
-        try:
-            employee_id = int(sys.argv[1])
-        except ValueError:
-            print("Invalid employee ID. Please provide a valid integer.")
-            sys.exit(1)
-
-        API = "https://jsonplaceholder.typicode.com"
-        user_res = requests.get(f"{API}/users/{employee_id}").json()
-        todos_res = requests.get(f"{API}/todos?userId={employee_id}").json()
-
-        user_name = user_res.get('name')
-
-        print(
-            f'Employee {user_name} is done with tasks({sum(1 for todo in todos_res if todo["completed"])}/{len(todos_res)}):'
-        )
-
-        for todo in todos_res:
-            if todo['completed']:
-                print(f'\t{todo["title"]}')
-
-        export_to_csv(employee_id, user_name, todos_res)
-    else:
-        print("Usage: python3 1-export_to_CSV.py <employee_id>")
+        if re.fullmatch(r'\d+', sys.argv[1]):
+            id = int(sys.argv[1])
+            user_res = requests.get('{}/users/{}'.format(API, id)).json()
+            todos_res = requests.get('{}/todos'.format(API)).json()
+            user_name = user_res.get('username')
+            todos = list(filter(lambda x: x.get('userId') == id, todos_res))
+            with open('{}.csv'.format(id), 'w') as file:
+                for todo in todos:
+                    file.write(
+                        '"{}","{}","{}","{}"\n'.format(
+                            id,
+                            user_name,
+                            todo.get('completed'),
+                            todo.get('title')
+                        )
+                    )

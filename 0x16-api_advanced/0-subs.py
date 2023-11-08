2227d735c516178676471e9b0fc511041d4df189
DIGeagh <darlingtongavi@gmail.com>
2023-11-08 13:39:51 -0800

    API ADVANCED

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,16 +1,38 @@
 #!/usr/bin/python3
-"""Function to query subscribers on a given Reddit subreddit."""
+"""
+0-subs
+"""
 import requests
 
-
 def number_of_subscribers(subreddit):
-    """Return the total number of subscribers on a given subreddit."""
-    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {
-        "User-Agent": "linux:0x16.api.advanced:v1.0.0"
-    }
-    response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code == 404:
+    # Reddit API URL for fetching subreddit information
+    url = f'https://www.reddit.com/r/{subreddit}/about.json'
+
+    # Set a custom User-Agent header to avoid Too Many Requests errors
+    headers = {'User-Agent': 'MyBot/1.0'}
+
+    try:
+        # Send an HTTP GET request to the Reddit API
+        response = requests.get(url, headers=headers)
+
+        # Check if the response status code is 200 (OK)
+        if response.status_code == 200:
+            data = response.json()
+            return data['data']['subscribers']
+        else:
+            # Return 0 if the subreddit is invalid or another error occurs
+            return 0
+
+    except Exception as e:
+        # Handle any exceptions that may occur during the request
         return 0
-    results = response.json().get("data")
-    return results.get("subscribers")
+
+if __name__ == '__main__':
+    import sys
+
+    if len(sys.argv) < 2:
+        print("Please pass an argument for the subreddit to search.")
+    else:
+        subreddit = sys.argv[1]
+        subscribers = number_of_subscribers(subreddit)
+        print(subscribers)

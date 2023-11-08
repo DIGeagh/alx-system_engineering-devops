1a918507b04c47822b90ec28f66570852f376430
DIGeagh <darlingtongavi@gmail.com>
2023-11-08 13:44:39 -0800

    API ADVANCED

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -5,26 +5,20 @@
 import requests
 
 def number_of_subscribers(subreddit):
-    # Reddit API URL for fetching subreddit information
     url = f'https://www.reddit.com/r/{subreddit}/about.json'
-
-    # Set a custom User-Agent header to avoid Too Many Requests errors
     headers = {'User-Agent': 'MyBot/1.0'}
 
     try:
-        # Send an HTTP GET request to the Reddit API
         response = requests.get(url, headers=headers)
 
-        # Check if the response status code is 200 (OK)
         if response.status_code == 200:
             data = response.json()
-            return data['data']['subscribers']
+            subscribers = data['data']['subscribers']
+            return subscribers
         else:
-            # Return 0 if the subreddit is invalid or another error occurs
             return 0
 
-    except Exception as e:
-        # Handle any exceptions that may occur during the request
+    except Exception:
         return 0
 
 if __name__ == '__main__':

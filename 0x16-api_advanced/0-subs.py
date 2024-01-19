e70b756eb6863887a65085b4c90a0e5ec2c42810
DIGeagh <darlingtongavi@gmail.com>
2024-01-18 17:02:50 -0800

    API Advanced

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -2,26 +2,20 @@
 """Function to query subscribers on a given Reddit subreddit."""
 import requests
 
+
 def number_of_subscribers(subreddit):
     """Return the total number of subscribers on a given subreddit."""
     try:
         url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
         headers = {
-            "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/Large_Alternative_30)"
+            "User-Agent": "linux:0x16.api.advanced:v1.0.0\
+            (by /u/Large_Alternative_30)"
         }
         response = requests.get(url, headers=headers, allow_redirects=False)
-        
         if response.status_code == 404:
             return 0
-        
         results = response.json().get("data")
         return results.get("subscribers")
-    except requests.exceptions.RequestException as e:
-        print(f"Error: {e}")
-        return 0
-
-# Example usage
-if __name__ == "__main__":
-    subreddit_name = input("Enter the subreddit name: ")
-    subscribers_count = number_of_subscribers(subreddit_name)
-    print(f"The number of subscribers for '{subreddit_name}' is: {subscribers_count}")
+    except (Exception):
+        print('Not Found')
+        return (0)

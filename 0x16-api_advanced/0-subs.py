f2342c1e62cef9ca08ab274e7cb7abf18a0b3b39
DIGeagh <darlingtongavi@hotmail.com>
2024-08-14 01:53:03 +0000

    API ADVANCE

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,22 +1,16 @@
 #!/usr/bin/python3
 """
-number of subscribers for a given subreddit
+Script that queries subscribers on a given Reddit subreddit.
 """
 
-from requests import get
+import requests
 
 
 def number_of_subscribers(subreddit):
-    """
-    function that queries the Reddit API and returns the number of subscribers
-    (not active users, total subscribers) for a given subreddit.
-    """
-
+    """Return the total number of subscribers on a given subreddit."""
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {
-        "User-Agent": "YourUserAgent"
-        }
-    response = get(url, headers=headers)
+    headers = {"User-Agent": "Mozilla/5.0"}
+    response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code == 200:
         data = response.json()
         subscribers = data['data']['subscribers']
